3c82926313883710aa7f0c24c9a5227dc08b0657
phelukas <ppedrolukas19@gmail.com>
2024-12-05 01:29:08 -0300

    logs

--- idus-backend/idus_backend/settings.py
+++ idus-backend/idus_backend/settings.py
@@ -6,6 +6,9 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:8000', "https://www.phelukas.com.br"]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
